62a9d83aeae51667636cf010daf90fc3edd5f29f
Jonathan Herbulot <jonathan.herbulot@scaffoldbid.com>
2025-01-07 07:38:29 +0100

    Start ui implementation of the calorie tracker page

--- src/app/components/calorie-tracker/calorie-tracker.component.ts
+++ src/app/components/calorie-tracker/calorie-tracker.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-calorie-tracker',
-  imports: [],
-  templateUrl: './calorie-tracker.component.html',
-  styleUrl: './calorie-tracker.component.scss'
+    selector: 'app-calorie-tracker',
+    imports: [],
+    templateUrl: './calorie-tracker.component.html',
+    styleUrl: './calorie-tracker.component.scss'
 })
 export class CalorieTrackerComponent {
+    public isAnimated: boolean = false;
 
+    constructor() { }
+
+    public ngOnInit(): void {
+        // Start the animation with a delay
+        setTimeout(() => {
+            this.isAnimated = true;
+        }, 500); // Adjust delay as needed
+    }
 }
